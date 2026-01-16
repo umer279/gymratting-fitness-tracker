afdc60707ddf3c13c094fc2459478a56acb56e6c
umer <umer.shahzaddar@subbyx.com>
2026-01-16 15:48:05 +0100

    chore: vite nev

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -3,7 +3,7 @@ import { createClient } from '@supabase/supabase-js';
 
 // These environment variables should be configured for the app to work.
 // You can find these in your Supabase project's API settings.
-const supabaseUrl = "";
-const supabaseAnonKey = "";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
